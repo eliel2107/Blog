56bc664eeb4918449d5139e8be04ea01ae5b80d0
Diogo Dieger <diogaodieger@gmail.com>
2024-01-15 10:47:33 -0300

    pages fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,15 +7,14 @@ import { ToastContainer } from 'react-toastify';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
-      <ToastContainer position="top-right" autoClose={5000} hideProgressBar />
       <Head>
         <link rel="icon" href="/FavIcon.svg" type="image/svg+xml" />
         <title>LW TECNOLOGIA</title>
         {/* Add other head elements if needed */}
       </Head>
-      <Component {...pageProps} />
       <NextNProgress color="#fff" />
+      <ToastContainer position="top-right" autoClose={5000} hideProgressBar />
+      <Component {...pageProps} />
     </>
   );
 }
