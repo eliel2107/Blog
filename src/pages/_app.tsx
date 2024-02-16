8764d0f10d19e09e4de6126c04e4463a14829d63
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-02-16 11:06:49 -0300

    pushing to dev

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,25 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
-import NextNProgress from 'nextjs-progressbar';
 import Head from 'next/head'; // Import the Head component
+import NextNProgress from 'nextjs-progressbar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; // Importe os estilos do react-toastify
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link rel="icon" href="/FavIcon.svg" type="image/svg+xml" />
-        <title>LW TECNOLOGIA</title>
-      </Head>
-      <NextNProgress color="#fff" />
-      <Component {...pageProps} />
-      <ToastContainer position="top-right" autoClose={5000} hideProgressBar />
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/FavIcon.svg" type="image/svg+xml" />
+                <title>LW TECNOLOGIA</title>
+            </Head>
+            <NextNProgress color="#fff" />
+            <Component {...pageProps} />
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar
+            />
+        </>
+    );
 }
+//
