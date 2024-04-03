e3476652e1087f0566645d06f2ba18731c17aede
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-04-03 11:32:38 -0300

    adding favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,53 +11,64 @@ import usePageTracking from '../../utils/usePageTracking';
 import { montserrat, poppins } from '../styles/fonts';
 import '../styles/globals.scss';
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
+    getLayout?: (page: ReactElement) => ReactNode;
 };
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
+    Component: NextPageWithLayout;
 };
 
 export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
+    Component,
+    pageProps: { session, ...pageProps },
 }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page);
+    const getLayout = Component.getLayout ?? ((page) => page);
 
-  useEffect(() => {
-    initializeTagManager();
-  }, []);
-  usePageTracking();
-  return (
-    <>
-      <Head>
-        <link rel="icon" href="/FavIcon.svg" type="image/svg+xml" />
-        <title>Grupo LW</title>
-        <meta property="og:title" content="Grupo LW" />
-        <meta
-          property="og:description"
-          content="Soluções em tecnologia para sua frota."
-        />
-        <meta property="og:site_name" content="Grupo LW" />
-        <meta property="og:url" content="https://lwtecnologia.com.br/" />
-        <meta
-          property="description"
-          content="Soluções em tecnologia para sua frota."
-        />
-        <meta
-          property="og:image"
-          content="https://d10fqir6n4h7sq.cloudfront.net/public/LOGOPRINCIPAL.png"
-        />
-        <meta property="og:image:width" content="304" />
-        <meta property="og:image:height" content="158" />
-      </Head>
-      <NextNProgress color="#fff" />
-      <div className={`${poppins.variable} ${montserrat.variable}`}>
-        {getLayout(<Component {...pageProps} />)}
-      </div>
-      <ToastContainer position="top-right" autoClose={5000} hideProgressBar />
-      <Cookies />
-    </>
-  );
+    useEffect(() => {
+        initializeTagManager();
+    }, []);
+    usePageTracking();
+    return (
+        <>
+            <Head>
+                <link
+                    rel="icon"
+                    href="https://d10fqir6n4h7sq.cloudfront.net/public/FavIcon.svg"
+                    type="image/svg+xml"
+                />
+                <title>Grupo LW</title>
+                <meta property="og:title" content="Grupo LW" />
+                <meta
+                    property="og:description"
+                    content="Soluções em tecnologia para sua frota."
+                />
+                <meta property="og:site_name" content="Grupo LW" />
+                <meta
+                    property="og:url"
+                    content="https://lwtecnologia.com.br/"
+                />
+                <meta
+                    property="description"
+                    content="Soluções em tecnologia para sua frota."
+                />
+                <meta
+                    property="og:image"
+                    content="https://d10fqir6n4h7sq.cloudfront.net/public/LOGOPRINCIPAL.png"
+                />
+                <meta property="og:image:width" content="304" />
+                <meta property="og:image:height" content="158" />
+            </Head>
+            <NextNProgress color="#fff" />
+            <div className={`${poppins.variable} ${montserrat.variable}`}>
+                {getLayout(<Component {...pageProps} />)}
+            </div>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar
+            />
+            <Cookies />
+        </>
+    );
 }
 //
