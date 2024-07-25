325b660ec190893c481a7058dc726e3ceafbd898
Leonardo <leolimadorea@gmail.com>
2024-07-25 14:35:21 -0300

    feat: colors matching

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,20 +1,33 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/Login.module.scss";
 
 export default function Login() {
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [visibleHr, setVisibleHr] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
 
   const handleImageClick = (index: any) => {
-    setVisibleHr(index);
+    setSelectedImage(index);
   };
 
-  const hrColors = ["red", "green", "blue", "purple"];
+  const buttonGradients = [
+    "linear-gradient(45deg, #ff4b2b, #ff416c, #ff4b2b, #ff416c)",
+    "linear-gradient(45deg, #11998e, #38ef7d, #11998e, #38ef7d)",
+    "linear-gradient(45deg, #f7971e, #ffd200, #f7971e, #ffd200)",
+  ];
+
+  useEffect(() => {
+    if (selectedImage !== null) {
+      document.documentElement.style.setProperty(
+        "--button-gradient",
+        buttonGradients[selectedImage]
+      );
+    }
+  }, [selectedImage]);
 
   return (
     <>
@@ -44,23 +57,31 @@ export default function Login() {
         <div className={styles.rightside}>
           <div className={styles.title}>
             <div className={styles.top}>
-              {["/doc.svg", "/debitos.svg", "/multas.svg", "/one.svg"].map(
-                (src, index) => (
-                  <div
-                    key={index}
-                    className={styles.imageContainer}
-                    onClick={() => handleImageClick(index)}
-                  >
-                    <img src={src} alt="" />
-                    {visibleHr === index && (
-                      <hr
-                        style={{ borderColor: hrColors[index] }}
-                        className={styles.horizontalLine}
-                      />
-                    )}
-                  </div>
-                )
-              )}
+              {["/doc.svg", "/debitos.svg", "/multas.svg"].map((src, index) => (
+                <div
+                  key={index}
+                  onClick={() => handleImageClick(index)}
+                  style={{ position: "relative", cursor: "pointer" }}
+                >
+                  <img src={src} />
+                  {selectedImage === index && (
+                    <hr
+                      style={{
+                        backgroundColor: ["#900", "#093", "#FC0"][index],
+                        height: "3px",
+                        borderRadius: "20px",
+                        border: "none",
+                        position: "absolute",
+                        bottom: "-5px",
+                        left: "0",
+                        right: "0",
+                        margin: "0 auto",
+                        width: "100%",
+                      }}
+                    />
+                  )}
+                </div>
+              ))}
             </div>
             <div className={styles.body}>
               <div className={styles.logoR}>
