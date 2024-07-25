d6f60bac26feef9bca424a89e7baa5802c158a02
DiogoDieger <diogaodieger@gmail.com>
2024-07-25 14:43:10 -0300

    login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import styles from "../styles/Login.module.scss";
+import axios from "axios";
 
 export default function Login() {
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(0);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [csrfToken, setCsrfToken] = useState("");
+  const [authCode, setAuthCode] = useState("");
+  const [step, setStep] = useState(1);
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
@@ -28,6 +34,146 @@ export default function Login() {
       );
     }
   }, [selectedImage]);
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    if (selectedImage === 0) {
+      await handleDocLogin();
+    } else if (selectedImage === 1) {
+      await handleDebitosLogin();
+    } else if (selectedImage === 2) {
+      await handleMultasLogin();
+    } else {
+      console.log("Other logins not implemented.");
+    }
+  };
+  const handleDocLogin = async () => {
+    try {
+      if (step === 1) {
+        const csrfResponse = await axios.get(
+          "https://documentacao.lwtecnologia.com.br/api/ms_usuarios/v1/usuario/csrf"
+        );
+        const csrfToken = csrfResponse.data.tokenCSRF;
+        setCsrfToken(csrfToken);
+        console.log(csrfToken);
+
+        const loginResponse = await axios.post(
+          "https://documentacao.lwtecnologia.com.br/api/ms_usuarios/v1/usuario/login",
+          {
+            email: email,
+            senha: password,
+            csrf: csrfToken,
+          }
+        );
+        const idEnvioEmail = loginResponse.data.idEnvioEmail;
+        console.log(idEnvioEmail);
+
+        setStep(2);
+      } else if (step === 2) {
+        const finalLoginResponse = await axios.post(
+          "https://documentacao.lwtecnologia.com.br/api/ms_usuarios/v1/usuario/login",
+          {
+            email: email,
+            senha: password,
+            codigo: authCode,
+            csrf: csrfToken,
+          }
+        );
+
+        const accessToken = finalLoginResponse.data.access_token;
+        console.log("Login successful!", accessToken);
+        window.location.href = `https://documentacao.lwtecnologia.com.br?token=${accessToken}`;
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
+  const handleDebitosLogin = async () => {
+    try {
+      if (step === 1) {
+        const csrfResponse = await axios.get(
+          "https://api.sistemamultas.com.br/v2/auth/csrf"
+        );
+        const csrfToken = csrfResponse.data.tokenCSRF;
+        setCsrfToken(csrfToken);
+        console.log(csrfToken);
+
+        const loginResponse = await axios.post(
+          "https://api.sistemamultas.com.br/v2/auth/plataforma",
+          {
+            email: email,
+            senha: password,
+            csrf: csrfToken,
+            plataforma: "DEBITOS",
+          }
+        );
+        const idEnvioEmail = loginResponse.data.idEnvioEmail;
+        console.log(idEnvioEmail);
+
+        setStep(2);
+      } else if (step === 2) {
+        const finalLoginResponse = await axios.post(
+          "https://api.sistemamultas.com.br/v2/auth/plataforma",
+          {
+            email: email,
+            senha: password,
+            codigo: authCode,
+            csrf: csrfToken,
+            plataforma: "DEBITOS",
+          }
+        );
+
+        const accessToken = finalLoginResponse.data.token;
+        console.log("Login successful!", accessToken);
+        window.location.href = `https://debitos.lwtecnologia.com.br?token=${accessToken}`;
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
+  const handleMultasLogin = async () => {
+    try {
+      if (step === 1) {
+        const csrfResponse = await axios.get(
+          "https://api.sistemamultas.com.br/v2/auth/csrf"
+        );
+        const csrfToken = csrfResponse.data.tokenCSRF;
+        setCsrfToken(csrfToken);
+        console.log(csrfToken);
+
+        const loginResponse = await axios.post(
+          "https://api.sistemamultas.com.br/v2/auth",
+          {
+            login: email,
+            senha: password,
+            csrf: csrfToken,
+          }
+        );
+        const idEnvioEmail = loginResponse.data.idEnvioEmail;
+        console.log(idEnvioEmail);
+
+        setStep(2);
+      } else if (step === 2) {
+        const finalLoginResponse = await axios.post(
+          "https://api.sistemamultas.com.br/v2/auth",
+          {
+            login: email,
+            senha: password,
+            codigo2FA: authCode,
+            csrf: csrfToken,
+          }
+        );
+
+        const accessToken = finalLoginResponse.data.token;
+        const urlValidar = finalLoginResponse.data.urlValidar;
+        console.log("Login successful!", accessToken);
+        window.location.href = urlValidar;
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
 
   return (
     <>
@@ -93,18 +239,42 @@ export default function Login() {
               <h1>Login</h1>
               <p>Informe seu acesso para entrar</p>
               <div className={styles.formblock}>
-                <form>
-                  <div className={styles.flexInput}>
-                    <p>Email</p>
-                    <input placeholder="Email" type="email" />
-                  </div>
-                  <div className={styles.flexInput}>
-                    <p>Senha</p>
-                    <input
-                      placeholder="Senha"
-                      type={passwordVisible ? "text" : "password"}
-                    />
-                  </div>
+                <form onSubmit={handleSubmit}>
+                  {step === 1 && (
+                    <>
+                      <div className={styles.flexInput}>
+                        <p>Email</p>
+                        <input
+                          placeholder="Email"
+                          type="email"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                      </div>
+                      <div className={styles.flexInput}>
+                        <p>Senha</p>
+                        <input
+                          placeholder="Senha"
+                          type={passwordVisible ? "text" : "password"}
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                      </div>
+                    </>
+                  )}
+                  {step === 2 && (
+                    <>
+                      <div className={styles.flexInput}>
+                        <p>Código de Autenticação</p>
+                        <input
+                          placeholder="Código de Autenticação"
+                          type="text"
+                          value={authCode}
+                          onChange={(e) => setAuthCode(e.target.value)}
+                        />
+                      </div>
+                    </>
+                  )}
                   <div className={styles.forgot}>
                     <Link href={"/recuperar"}>
                       <p>Esqueci minha senha</p>
