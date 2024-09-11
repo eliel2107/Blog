60a50b08fe5221f58a0a61e528b9d4a42265b227
DiogoDieger <ddiegerfernandes@gmail.com>
2024-09-11 11:42:08 -0300

    feat: app policy page

---
+++ src/pages/appprivacy.tsx
@@ -0,0 +1,154 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import styles from "../styles/termos.module.scss";
+
+export default function AppPolicy() {
+  return (
+    <>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <h1>POLÍTICA DE APPS</h1>
+          <div className={styles.content}>
+            <p>Última atualização: 11 de setembro de 2024.</p>
+            <p>
+              Esta Política de Privacidade descreve como coletamos, usamos e
+              compartilhamos suas informações ao utilizar o nosso aplicativo
+              disponível na Google Play Store ou Apple Store operada pela LW
+              Tecnologia Ltda. (&quot;LW Tecnologia&quot;) CNPJ:
+              44.383.200/0001-46. Ao utilizar nossos serviços, você concorda com
+              esta política. Caso não concorde, a utilização dos nossos serviços
+              deverá ser interrompida e o DPO deverá ser notificado por meio do
+              e-mail informado nesta política.
+            </p>
+            <p>1. Informações que coletamos</p>
+            <h1>1.1. Informações Pessoais</h1>
+            <p>
+              Podemos coletar informações que você nos fornece diretamente, como
+              nome, e-mail, número de telefone, endereço, CEP, dados de
+              pagamento e outras informações de contato.
+            </p>
+            <h1>1.2. Informações Coletadas Automaticamente</h1>
+            <p>
+              Ao utilizar nossos serviços, podemos coletar automaticamente
+              certas informações como:
+            </p>
+
+            <p>
+              Dados de uso (interações com o aplicativo ou a plataforma,
+              preferências);
+            </p>
+            <p>
+              Informações do dispositivo (tipo de dispositivo, sistema
+              operacional, identificador único);
+            </p>
+            <p>Dados de localização, se permitido por você.</p>
+
+            <h1>
+              1.3. Informações Coletadas por Cookies e Tecnologias Semelhantes
+            </h1>
+            <p>
+              Utilizamos cookies e tecnologias semelhantes para personalizar a
+              experiência do usuário, autenticação, registro de produtos no
+              carrinho e aprimorar nossos serviços. Cookies podem ser
+              persistentes ou de sessão. Você pode gerenciar as configurações de
+              cookies no seu navegador.
+            </p>
+            <h2>2. Uso das Informações</h2>
+            <p>Utilizamos as informações coletadas para:</p>
+
+            <p>Processar pedidos e melhorar a experiência do usuário;</p>
+            <p>Manter e aprimorar o aplicativo e a plataforma;</p>
+            <p>
+              Comunicar atualizações, responder a dúvidas e oferecer suporte;
+            </p>
+            <p>Cumprir obrigações legais e regulatórias.</p>
+
+            <h1>3. Compartilhamento de Informações</h1>
+            <p>
+              Não vendemos suas informações pessoais. Podemos compartilhar suas
+              informações nas seguintes circunstâncias:
+            </p>
+
+            <p>
+              Com fornecedores de serviços que ajudam a operar o aplicativo ou a
+              plataforma;
+            </p>
+            <p>
+              Para cumprir com obrigações legais, ordens judiciais ou processos
+              legais;
+            </p>
+            <p>
+              Para proteger nossos direitos, propriedade e segurança, bem como
+              dos nossos usuários.
+            </p>
+
+            <h2>4. Segurança</h2>
+            <p>
+              Implementamos todas as medidas de segurança necessárias, como
+              criptografia de dados sensíveis, para proteger suas informações
+              contra acesso não autorizado, perda, alteração ou divulgação.
+            </p>
+            <h2>5. Retenção de Dados</h2>
+            <p>
+              Manteremos suas informações enquanto for necessário para cumprir
+              as finalidades descritas nesta política. Em algumas
+              circunstâncias, os dados podem ser mantidos por um período
+              indeterminado, conforme exigido ou permitido por lei. Você pode
+              solicitar a exclusão dos dados pelo e-mail de contato informado
+              nesta política.
+            </p>
+            <h2>6. Seus Direitos</h2>
+            <p>
+              Dependendo da sua localização, você pode ter direitos sobre suas
+              informações, como o direito de acessar, corrigir ou excluir seus
+              dados pessoais. Para exercer esses direitos, entre em contato
+              conosco.
+            </p>
+            <h2>7. Revogação de Consentimento</h2>
+            <p>
+              Após aceitar esta Política de Privacidade, você não poderá revogar
+              o consentimento para o processamento dos dados enquanto utilizar
+              nossos serviços. O consentimento poderá ser revogado ao solicitar
+              a exclusão da conta e interrupção do uso.
+            </p>
+            <h2>8. Alterações nesta Política de Privacidade</h2>
+            <p>
+              Reservamo-nos o direito de atualizar esta Política de Privacidade
+              periodicamente. Quaisquer alterações serão publicadas nesta página
+              ou na plataforma e entrarão em vigor imediatamente após a
+              publicação.
+            </p>
+            <h2>9. Obrigações do Usuário</h2>
+            <p>Os usuários devem:</p>
+
+            <p>
+              Manter suas credenciais de acesso (e-mail ou telefone) e códigos
+              OTP em segurança;
+            </p>
+            <p>
+              Fornecer informações reais e atualizadas nos campos cadastrais da
+              plataforma;
+            </p>
+            <p>
+              Efetuar o pagamento integral dos produtos e taxas conforme
+              solicitado.
+            </p>
+
+            <h2>10. Contato</h2>
+            <p>
+              Se você tiver dúvidas ou preocupações sobre esta Política de
+              Privacidade, entre em contato conosco:
+            </p>
+            <p>
+              <strong>Grupo LW</strong>
+              <br />
+              dpo@lwtecnologia.com.br
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
